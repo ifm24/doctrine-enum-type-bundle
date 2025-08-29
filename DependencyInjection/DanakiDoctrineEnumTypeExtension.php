3796e04fc0f31606457e1bf1d25bafb3544d0e0c
Ondřej Nedvídek <ondrej.nedvidek@starkys.software>
2025-08-29 16:40:15 +0200

    Update - Configuration based on Symfony 7 @2

--- DependencyInjection/DanakiDoctrineEnumTypeExtension.php
+++ DependencyInjection/DanakiDoctrineEnumTypeExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
 class DanakiDoctrineEnumTypeExtension extends ConfigurableExtension
 {
-    protected function loadInternal(array $config, ContainerBuilder $container)
+    protected function loadInternal(array $config, ContainerBuilder $container): void
     {
         $container->setParameter('danaki_doctrine_enum_type.types', $config['types']);
     }
